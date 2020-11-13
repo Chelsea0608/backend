60a50d6717be52c33e8e2902ef89a5ca88d3283e
Chelsea0608 <55235696+Chelsea0608@users.noreply.github.com>
2020-11-13 09:40:11 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,21 @@
+/*const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    firstname:{
+        type: Number
+       
+    },
+    lastname:{
+        type: Number
+        
+    },
+    password:{
+        type: String,
+        required: true
+    },
+})
+module.exports = mongoose.model('user',UserSchema);*/
